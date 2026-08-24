3419f65bc3247b51c193e08e34840f4b6bdb076a
Ayabee-creator <s225955857@mandela.ac.za>
2026-08-24 15:36:25 +0200

    Updated but not finished

--- app/src/main/java/com/example/getyourride/MainActivity.kt
+++ app/src/main/java/com/example/getyourride/MainActivity.kt
@@ -895,7 +895,8 @@ class MainActivity : ComponentActivity() {
                     composable("shuttle_driver_boarding") {
                         val boardingViewModel: ShuttleDriverBoardingViewModel = viewModel(
                             factory = ShuttleDriverBoardingViewModelFactory(
-                                ShuttleDriverRepository(NetworkModule.shuttleDriverApi)
+                                ShuttleDriverRepository(NetworkModule.shuttleDriverApi),
+                                NetworkModule.tripApi
                             )
                         )
 
@@ -905,6 +906,9 @@ class MainActivity : ComponentActivity() {
                             onLoadData = { boardingViewModel.loadBoardingData() },
                             onMarkAsBoarded = { bookingId ->
                                 boardingViewModel.markStudentAsBoarded(bookingId)
+                            },
+                            onSelectTimeSlot = { slot ->
+                                boardingViewModel.selectTimeSlot(slot)
                             },
                             onScanQrCodeClick = {
                                 navController.navigate("shuttle_driver_scan_qr") { launchSingleTop = true }
